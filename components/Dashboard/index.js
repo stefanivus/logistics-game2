4e19c80d9b8e818ffb6d03fc96b2d78e89a13745
Justin Chang <JustinChangMusic@gmail.com>
2017-02-27 10:12:18 -0500

    Laid out what I need for Dashboard

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -5,7 +5,10 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className={"mdl-grid " + s.border}>
-        <div className="mdl-cell mdl-cell--1-col">1</div>
+        <div className="mdl-cell mdl-cell--4-col">Company Information</div>
+        <div className="mdl-cell mdl-cell--2-col">Vehicle List</div>
+        <div className="mdl-cell mdl-cell--2-col">Employee List</div>
+        <div className="mdl-cell mdl-cell--2-col">Deliveries in Queue</div>
       </div>
     );
   }
