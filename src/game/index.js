ed3aa0bb20537f46db4f4b16d957036e9800a39d
Justin Chang <JustinChangMusic@gmail.com>
2017-02-27 09:54:11 -0500

    Have a basic idea of what I want the home page to say
    Now onto actually making the game, the important part

---
+++ src/game/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+class Logistics extends React.Component {
+  render() {
+    return (
+      <Layout>
+
+        <h3>Client List - with smiley face </h3>
+
+        <h1>Map</h1>
+
+        <h3>Company Dashboard</h3>
+
+      </Layout>
+    );
+  }
+}
+
+export default Logistics;
--- src/home/index.js
+++ src/home/index.js
@@ -1,6 +1,7 @@
-import React, { PropTypes } from 'react';
-import Layout from '../../components/Layout';
-import s from './styles.css';
+import React, { PropTypes } from 'react'
+import Link from '../../components/Link'
+import Layout from '../../components/Layout'
+import s from './styles.css'
 
 class HomePage extends React.Component {
 
@@ -12,6 +13,14 @@ class HomePage extends React.Component {
     return (
       <Layout className={s.content}>
         <h1 className={s.centerText}>Welcome to Logistics!</h1>
+
+        {/* Can change this from a <Link> later */}
+        <Link to="/game"
+          className={s.bigText}>Let's Play Button</Link>
+
+        <div className={s.bigText}>How to Play</div>
+
+        <div className={s.bigText}>Purpose of the Game</div>
 
       </Layout>
     );
