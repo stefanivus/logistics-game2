4da628236d57e4665b6ceb863e531670df41e43d
Justin Chang <JustinChangMusic@gmail.com>
2017-02-28 01:07:19 -0500

    configuring dashboard to show different content on click

--- components/GameDashboard/index.js
+++ components/GameDashboard/index.js
@@ -5,7 +5,7 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className={s.dashContainer}>
-
+        
         <h1>Dashboard</h1>
 
         <div>
--- src/game/index.js
+++ src/game/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 
-import GameOverlays from '../../components/Layout/GameOverlays'
+import GameOverlays from './components/GameOverlay'
 import CompanyDashboard from '../../components/GameDashboard'
 import Map from './components/Map/Map'
 
