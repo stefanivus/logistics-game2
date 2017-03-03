a380681f8df55c1ef22afe4b248e4715f9fd462c
Justin Chang <JustinChangMusic@gmail.com>
2017-03-03 10:40:33 -0500

    Record time build

---
+++ src/game/old-comp/GameLayout/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import s from './Header.css';
+
+import Link from '../../../../components/Link';
+
+class Header extends React.Component {
+
+  componentDidMount() {
+    window.componentHandler.upgradeElement(this.root);
+  }
+
+  componentWillUnmount() {
+    window.componentHandler.downgradeElements(this.root);
+  }
+
+  render() {
+    return (
+      <header className={`mdl-layout__header ${s.header}`} ref={node => (this.root = node)}>
+        <div className={`mdl-layout__header-row ${s.row}`}>
+
+          <Link className={`mdl-layout-title ${s.title}`} to="/">
+            Logistics
+          </Link>
+
+          <div className="mdl-layout-spacer" />
+
+          <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+          </nav>
+          
+
+        </div>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
---
+++ src/game/old-comp/GameLayout/Layout.js
@@ -0,0 +1,50 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import Header from './Header';
+import Drawer from './Drawer';
+// import Footer from '../Footer';
+import s from './Layout.css';
+
+class GameLayout extends React.Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    className: "",
+  };
+
+  /* These are handlers used by MDL */
+  componentDidMount() {
+    window.componentHandler.upgradeElement(this.root);
+  }
+
+  componentWillUnmount() {
+    window.componentHandler.downgradeElements(this.root);
+  }
+
+  render() {
+    return (
+      <div className="mdl-layout mdl-js-layout" ref={node => (this.root = node)}>
+        <div className="mdl-layout__inner-container">
+
+          <Header />
+          <Drawer title="Company Dashboard"></Drawer>
+
+          <main className="mdl-layout__content" style={{flex: '1 0 auto'}}>
+            {/* Child Componenta */}
+            <div {...this.props} className={this.props.className}></div>
+
+            {/* <Footer /> */}
+
+          </main>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+        export default GameLayout;
